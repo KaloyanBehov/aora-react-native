82f692e0dd29554b471d9ddc2b40719213422c7a
Kaloyanbehov <kaloqnbehov@gmail.com>
2024-06-17 19:40:45 +0300

    tab bar navigation menu

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,12 +1,102 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Image,Text } from 'react-native';
+import React from 'react';
+import { Tabs } from 'expo-router';
+import { icons } from '../../constants';  // Make sure this path is correct
+
+const TabIcon = ({ icon, color,name, focused }) => {
+  return (
+    <View className='items-center justify-center gap-2'>
+      <Image 
+        source={icon} 
+        resizeMode='contain' 
+        style={{ width: 24, height: 24, tintColor: color }} // Ensure the style is correctly applied
+      />
+      <Text className={`${focused ? 'font-bold' : 'font-medium'} text-xs`} style={{color:color}}>
+        {name}
+      </Text>
+    </View>
+  );
+};
 
 const TabsLayout = () => {
   return (
-    <View>
-      <Text>TabsLayout</Text>
-    </View>
-  )
-}
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: '#FFA001',
+          tabBarInactiveTintColor: '#CDCDE0',
+          tabBarStyle: {
+            backgroundColor: '#161622',
+            borderTopWidth: 1,
+            borderTopColor: '#232533',
+            height:84,
+          }
+      }}
+      >
+        <Tabs.Screen 
+          name='home' 
+          options={{
+            title: 'Home',
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (  // Use tabBarIcon instead of tabBarIcons
+              <TabIcon 
+                icon={icons.home} 
+                color={color} 
+                name={'Home'} 
+                focused={focused} 
+              />
+            )
+          }} 
+        />
+            <Tabs.Screen 
+          name='bookmark' 
+          options={{
+            title: 'Bookmark',
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (  // Use tabBarIcon instead of tabBarIcons
+              <TabIcon 
+                icon={icons.bookmark} 
+                color={color} 
+                name={'Bookmark'} 
+                focused={focused} 
+              />
+            )
+          }} 
+        />
+            <Tabs.Screen 
+          name='create' 
+          options={{
+            title: 'Create',
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (  // Use tabBarIcon instead of tabBarIcons
+              <TabIcon 
+                icon={icons.plus} 
+                color={color} 
+                name={'Create'} 
+                focused={focused} 
+              />
+            )
+          }} 
+        />
+              <Tabs.Screen 
+          name='profile' 
+          options={{
+            title: 'Profile',
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (  // Use tabBarIcon instead of tabBarIcons
+              <TabIcon 
+                icon={icons.profile} 
+                color={color} 
+                name={'Profile'} 
+                focused={focused} 
+              />
+            )
+          }} 
+        />
+      </Tabs>
+    </>
+  );
+};
 
-export default TabsLayout
+export default TabsLayout;
